6de180f58244db66e9d9cae2078407bd41cf5e13
Victor Azevedo <victor.azevedo@mail.com>
2023-07-25 18:02:50 +0200

    Oopsie with arguments

--- netlify/edge-functions/geol.ts
+++ netlify/edge-functions/geol.ts
@@ -1,26 +1,5 @@
 import { Context } from 'https://edge.netlify.com'
 
-export default (context: Context) => {
-	// Here's what's available on context.geo
-
-	// context: {
-	//   geo: {
-	//     city?: string;
-	//     country?: {
-	//       code?: string;
-	//       name?: string;
-	//     },
-	//     subdivision?: {
-	//       code?: string;
-	//       name?: string;
-	//     },
-	//     latitude?: number;
-	//     longitude?: number;
-	//     timezone?: string;
-	//   }
-	// }
-
-	return Response.json({
-		geo: context.geo,
-	})
+export default (_: Request, context: Context) => {
+	return Response.json({ ...context.geo })
 }
